a4e23673c3250aeb82ecc62447247765ea84f8c3
Hyllesen <stefanhyltoft@gmail.com>
2021-05-18 17:50:23 +0200

    Not tracking pods anymore

--- ios/Pods/Target Support Files/Pods-skywriterMDMobile-skywriterMDMobileTests/Pods-skywriterMDMobile-skywriterMDMobileTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_skywriterMDMobile_skywriterMDMobileTests : NSObject
-@end
-@implementation PodsDummy_Pods_skywriterMDMobile_skywriterMDMobileTests
-@end
